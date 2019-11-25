5b3edee8c91034c9d2d26d2d120a2049aea7234d
vjoki <vjoki@zv.fi>
2019-11-25 20:00:53 +0200

    Remove unused env_logger

--- examples/basic_bytes.rs
+++ examples/basic_bytes.rs
@@ -1,11 +1,9 @@
-extern crate env_logger;
 extern crate unrar;
 
 use std::str;
 use unrar::Archive;
 
 fn main() {
-    env_logger::init().unwrap();
     println!(
         "{}",
         str::from_utf8(&Archive::new("version.rar".into())
