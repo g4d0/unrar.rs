be02926db717af19824500665fe82db83de85070
vjoki <vjoki@zv.fi>
2019-12-09 19:49:55 +0200

    Add tests for archive comment

---
+++ tests/comment.rs
@@ -0,0 +1,22 @@
+extern crate unrar;
+
+use unrar::Archive;
+use unrar::error::{Code, When};
+
+#[test]
+fn archive_comment() {
+    let mut archive = Archive::new("data/comment.rar");
+    archive.enable_comments();
+    let open_archive = archive.list().unwrap();
+    assert!(open_archive.has_comment());
+    assert_eq!(open_archive.comment().unwrap(), "abcdef12345\n");
+}
+
+#[test]
+fn archive_comment_crypted() {
+    let mut archive = Archive::with_password("data/comment-hpw-password.rar", "password");
+    archive.enable_comments();
+    let open_archive = archive.list().unwrap();
+    assert!(open_archive.has_comment());
+    assert_eq!(open_archive.comment().unwrap(), "abcdef12345\n");
+}
