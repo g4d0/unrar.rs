6059587b0da90746a5679c03678503da10e4d0d9
vjoki <vjoki@zv.fi>
2020-01-18 15:57:30 +0200

    Sync to api changes

--- tests/comment.rs
+++ tests/comment.rs
@@ -4,7 +4,7 @@ use unrar::Archive;
 
 #[test]
 fn archive_comment() {
-    let mut archive = Archive::new("data/comment.rar");
+    let mut archive = Archive::new("data/comment.rar").unwrap();
     archive.enable_comments();
     let open_archive = archive.list().unwrap();
     assert_eq!(open_archive.comment().unwrap(), "abcdef12345\n");
@@ -13,14 +13,14 @@ fn archive_comment() {
 // // Requires that UCM_NEEDPASSWORDW is implemented.
 // #[test]
 // fn archive_comment_crypted() {
-//     let mut archive = Archive::with_password("data/comment-hpw-password.rar", "password");
+//     let mut archive = Archive::with_password("data/comment-hpw-password.rar", "password").unwrap();
 //     archive.enable_comments();
 //     let open_archive = archive.list().unwrap();
 //     assert_eq!(open_archive.comment().unwrap(), "abcdef12345\n");
 
 // #[test]
 // fn utf8_comment() {
-//     let mut archive = Archive::new("data/comment-utf8.rar");
+//     let mut archive = Archive::new("data/comment-utf8.rar").unwrap();
 //     archive.enable_comments();
 //     let open_archive = archive.list().unwrap();
 //     assert!(open_archive.has_comment());
